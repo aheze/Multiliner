6e7b3e2a363c4442d64f314638c88b61ced06c61
Zheng <aheze@getfind.app>
2022-06-27 12:40:15 -0700

    Initial Commit

---
+++ Multiliner/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Multiliner
+//
+//  Created by A. Zheng (github.com/aheze) on 6/27/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Multiliner/MultilinerApp.swift
@@ -0,0 +1,19 @@
+//
+//  MultilinerApp.swift
+//  Multiliner
+//
+//  Created by A. Zheng (github.com/aheze) on 6/27/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct MultilinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
