dbe5a7b6684ea4d93bc2ff9560fe230cae3b8f33
Zheng <aheze@getfind.app>
2022-07-05 20:49:50 -0700

    Fix #9 and #10

--- Sources/MultilinerExtension/SourceEditorCommand.swift
+++ Sources/MultilinerExtension/SourceEditorCommand.swift
@@ -10,175 +10,184 @@ import Foundation
 import XcodeKit
 
 enum FormatError: Error, CustomStringConvertible, LocalizedError, CustomNSError {
-	case noSelection
-	case invalidSelection
+    case noSelection
+    case invalidSelection
 
-	var description: String {
-		switch self {
-		case .noSelection:
-			return "No selection."
-		case .invalidSelection:
-			return "Selection must be bounded by `()` or `[]`."
-		}
-	}
+    var description: String {
+        switch self {
+        case .noSelection:
+            return "No selection."
+        case .invalidSelection:
+            return "Selection must be bounded by `()` or `[]`."
+        }
+    }
 
-	var localizedDescription: String {
-		return "Error: \(description)."
-	}
+    var localizedDescription: String {
+        return "Error: \(description)."
+    }
 
-	var errorUserInfo: [String: Any] {
-		return [NSLocalizedDescriptionKey: localizedDescription]
-	}
+    var errorUserInfo: [String: Any] {
+        return [NSLocalizedDescriptionKey: localizedDescription]
+    }
 }
 
 enum SelectionKind {
-	case parameters
-	case array
+    case parameters
+    case array
 }
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
-	/// The `Format Selection` command.
-	func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
-		/// Get the selection first.
-		guard
-			let selection = invocation.buffer.selections.firstObject,
-			let range = selection as? XCSourceTextRange
-		else {
-			completionHandler(FormatError.noSelection)
-			return
-		}
+    /// The `Format Selected Code` command.
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
+        /// Get the selection first.
+        guard
+            let selection = invocation.buffer.selections.firstObject,
+            let range = selection as? XCSourceTextRange
+        else {
+            completionHandler(FormatError.noSelection)
+            return
+        }
 
-		/// It's possible that the user selected the last "extra" line too.
-		if range.start.line > invocation.buffer.lines.count - 1 { range.start.line -= 1 }
-		if range.end.line > invocation.buffer.lines.count - 1 { range.end.line -= 1 }
+        /// It's possible that the user selected the last "extra" line too.
+        if range.start.line > invocation.buffer.lines.count - 1 { range.start.line -= 1 }
+        if range.end.line > invocation.buffer.lines.count - 1 { range.end.line -= 1 }
 
-		/// Store the current lines of the entire file.
-		let oldLines = getLines(from: invocation.buffer)
+        /// Store the current lines of the entire file.
+        let oldLines = getLines(from: invocation.buffer)
 
-		/// The selection's starting tab.
-		/// Example:
-		//     **input**  `    init()`
-		//     **output** `    `
-		let startTab: Substring
+        /// The width of a single tab, usually `    `.
+        let tab: String
 
-		/// The width of a single tab, usually `    `.
-		let tab: String
+        /// The selection's starting tab.
+        /// Example:
+        //     **input**  `    init()`
+        //     **output** `    `
+        let startTab: Substring
 
-		if invocation.buffer.usesTabsForIndentation {
-			startTab = oldLines[range.start.line]
-				.prefix { $0 == "\u{0009}" }
+        let startColumn: Int
 
-			tab = "\u{0009}" /// Tab character.
-		} else {
-			startTab = oldLines[range.start.line]
-				.prefix { $0 == " " }
+        if invocation.buffer.usesTabsForIndentation {
+            tab = "\u{0009}" /// Tab character.
 
-			tab = String(repeating: " ", count: invocation.buffer.indentationWidth)
-		}
+            startTab = oldLines[range.start.line]
+                .prefix { $0 == "\u{0009}" }
 
-		/// The tab that prefixes each parameter/array element.
-		let contentTab = startTab + tab
+            startColumn = startTab.count
 
-		/// The entire text of the file.
-		let text = getText(from: range, buffer: invocation.buffer)
+        } else {
+            startTab = oldLines[range.start.line]
+                .prefix { $0 == " " }
 
-		/// Get the opening and closing indices if the selected text contains parameters.
-		let openingParenthesisIndex = text.firstIndex(of: "(")
-		let closingParenthesisIndex = text.lastIndex(of: ")")
+            tab = String(repeating: " ", count: invocation.buffer.indentationWidth)
 
-		/// Get the opening and closing array element if the selected text is an array.
-		let openingArrayIndex = text.firstIndex(of: "[")
-		let closingArrayIndex = text.lastIndex(of: "]")
+            startColumn = startTab.count / invocation.buffer.indentationWidth
+        }
 
-		/// Determine if the selection was an array or a set of parameters.
-		/// Only use the opening brace for comparison.
-		var selectionKind: SelectionKind
-		switch (openingParenthesisIndex, openingArrayIndex) {
-		case let (.some(openingParenthesisIndex), .some(openingArrayIndex)):
-			if openingParenthesisIndex < openingArrayIndex {
-				selectionKind = .parameters
-			} else {
-				selectionKind = .array
-			}
-		case (.some, .none):
-			selectionKind = .parameters
-		case (.none, .some):
-			selectionKind = .array
-		default:
-			completionHandler(FormatError.noSelection)
-			return
-		}
+        /// The tab that prefixes each parameter/array element.
+        let contentTab = startTab + tab
 
-		/// Determine if the selection was an array or a set of parameters.
-		let openingBracesIndex: String.Index? = selectionKind == .parameters
-			? openingParenthesisIndex
-			: openingArrayIndex
+        /// The entire text of the file.
+        let text = getText(from: range, buffer: invocation.buffer)
 
-		let closingBracesIndex: String.Index? = selectionKind == .parameters
-			? closingParenthesisIndex
-			: closingArrayIndex
+        /// Get the opening and closing indices if the selected text contains parameters.
+        let openingParenthesisIndex = text.firstIndex(of: "(")
+        let closingParenthesisIndex = text.lastIndex(of: ")")
 
-		/// Make sure there's an opening and closing index.
-		guard let openingBracesIndex = openingBracesIndex, let closingBracesIndex = closingBracesIndex else {
-			completionHandler(FormatError.invalidSelection)
-			return
-		}
+        /// Get the opening and closing array element if the selected text is an array.
+        let openingArrayIndex = text.firstIndex(of: "[")
+        let closingArrayIndex = text.lastIndex(of: "]")
 
-		/// Skip the opening `(` or `[`.
-		let openingContentIndex = text.index(after: openingBracesIndex)
-		let closingContentIndex = closingBracesIndex
+        /// Determine if the selection was an array or a set of parameters.
+        /// Only use the opening brace for comparison.
+        var selectionKind: SelectionKind
+        switch (openingParenthesisIndex, openingArrayIndex) {
+        case let (.some(openingParenthesisIndex), .some(openingArrayIndex)):
+            if openingParenthesisIndex < openingArrayIndex {
+                selectionKind = .parameters
+            } else {
+                selectionKind = .array
+            }
+        case (.some, .none):
+            selectionKind = .parameters
+        case (.none, .some):
+            selectionKind = .array
+        default:
+            completionHandler(FormatError.noSelection)
+            return
+        }
 
-		/// The text inside the braces.
-		let contentsString = text[openingContentIndex ..< closingContentIndex]
-		let contents = contentsString
-			.components(separatedBy: ",")
+        /// Determine if the selection was an array or a set of parameters.
+        let openingBracesIndex: String.Index? = selectionKind == .parameters
+            ? openingParenthesisIndex
+            : openingArrayIndex
 
-		/// Format the content by adding spaces and commas.
-		let contentsFormatted: [String] = contents.enumerated()
-			.map { index, element in
-				let line = element.trimmingCharacters(in: .whitespacesAndNewlines)
-				if index == contents.indices.last {
-					return contentTab + line
-				} else {
-					return contentTab + line + ","
-				}
-			}
+        let closingBracesIndex: String.Index? = selectionKind == .parameters
+            ? closingParenthesisIndex
+            : closingArrayIndex
 
-		/// The string that comes before the selection.
-		let openingString = text[..<openingContentIndex]
-		let closingString = startTab + text[closingContentIndex...] /// add start tab padding
+        /// Make sure there's an opening and closing index.
+        guard let openingBracesIndex = openingBracesIndex, let closingBracesIndex = closingBracesIndex else {
+            completionHandler(FormatError.invalidSelection)
+            return
+        }
 
-		/// The new lines of the entire file.
-		let newLines = [openingString] + contentsFormatted + [closingString]
+        /// Skip the opening `(` or `[`.
+        let openingContentIndex = text.index(after: openingBracesIndex)
+        let closingContentIndex = closingBracesIndex
 
-		let openingLines = oldLines[..<range.start.line]
-		let closingLines = oldLines[(range.end.line + 1)...]
-		let lines = Array(openingLines) + Array(newLines) + Array(closingLines)
+        /// The text inside the braces.
+        let contentsString = text[openingContentIndex ..< closingContentIndex]
+        let contents = contentsString
+            .components(separatedBy: ",")
 
-		/// Update the source code.
-		invocation.buffer.lines.removeAllObjects()
-		invocation.buffer.lines.addObjects(from: lines)
+        /// Format the content by adding spaces and commas.
+        let contentsFormatted: [String] = contents.enumerated()
+            .map { index, element in
+                let line = element.trimmingCharacters(in: .whitespacesAndNewlines)
+                if index == contents.indices.last {
+                    return contentTab + line
+                } else {
+                    return contentTab + line + ","
+                }
+            }
 
-		invocation.buffer.selections.removeAllObjects()
-		invocation.buffer.selections.add(
-			range
-		)
+        /// The string that comes before the selection.
+        let openingString = text[..<openingContentIndex]
+        let closingString = startTab + text[closingContentIndex...] /// add start tab padding
 
-		/// Success!
-		completionHandler(nil)
-	}
+        /// The new lines of the entire file.
+        let newLines = [openingString] + contentsFormatted + closingString.split(separator: "\n")
 
-	/// Get the lines of an entire files as an array of `String`s.
-	func getLines(from buffer: XCSourceTextBuffer) -> [String] {
-		guard let lines = buffer.lines as? [String] else { return [] }
-		return lines
-	}
+        /// The range in the existing lines that were modified.
+        let oldRange = NSRange(location: range.start.line, length: range.end.line - range.start.line + 1)
 
-	/// Get a single string from a range.
-	func getText(from range: XCSourceTextRange, buffer: XCSourceTextBuffer) -> String {
-		let allLines = getLines(from: buffer)
-		let lines = allLines[range.start.line ... range.end.line]
-		let text = lines.map { String($0) }.joined()
-		return text
-	}
+        /// Update the source code.
+        invocation.buffer.lines.replaceObjects(in: oldRange, withObjectsFrom: newLines)
+
+        /// Update the selection to encompass the new text.
+        invocation.buffer.selections.removeAllObjects()
+        invocation.buffer.selections.add(
+            XCSourceTextRange(
+                start: XCSourceTextPosition(line: range.start.line, column: startColumn),
+                end: XCSourceTextPosition(line: range.start.line + newLines.count - 1, column: closingString.count - 1)
+            )
+        )
+
+        /// Success!
+        completionHandler(nil)
+    }
+
+    /// Get the lines of an entire files as an array of `String`s.
+    func getLines(from buffer: XCSourceTextBuffer) -> [String] {
+        guard let lines = buffer.lines as? [String] else { return [] }
+        return lines
+    }
+
+    /// Get a single string from a range.
+    func getText(from range: XCSourceTextRange, buffer: XCSourceTextBuffer) -> String {
+        let allLines = getLines(from: buffer)
+        let lines = allLines[range.start.line ... range.end.line]
+        let text = lines.map { String($0) }.joined()
+        return text
+    }
 }
