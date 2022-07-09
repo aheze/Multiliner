8f6f6e30e2a09680569e7aa9ab7e154a3a39f630
Zheng <aheze@getfind.app>
2022-07-09 13:14:05 -0700

    Update app formatting

--- Sources/Multiliner/ContentView.swift
+++ Sources/Multiliner/ContentView.swift
@@ -11,80 +11,104 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 20) {
                 Image("Banner")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-
-                Text("Multiliner")
-                    .font(.largeTitle)
-                    .bold()
-
-                Text("An Xcode source extension to expand lengthy lines.")
-                    .font(.title)
-                    .fontWeight(.medium)
-                    .foregroundColor(.secondary)
-
-                Divider()
-
-                Group {
-                    Text("Installation")
-                        .font(.title2)
+                
+                VStack(alignment: .leading, spacing: 6) {
+                    Text("Multiliner")
+                        .font(.largeTitle)
                         .bold()
-
-                    Text("Now that you have the app installed, just head over to System Preferences and check `MultilinerExtension`.")
-                        .font(.title3)
-
-                    Image("Preferences")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+                    
+                    Text("An Xcode source extension to expand lengthy lines.")
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .foregroundColor(.secondary)
                 }
-
-                Group {
-                    Group {
-                        Text("Usage")
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    VStack(alignment: .leading) {
+                        Text("Installation")
                             .font(.title2)
                             .bold()
-
-                        Text("1. Select some code in Xcode. Make sure to include the opening and closing braces.")
+                    
+                        Text("Now that you have the app installed, just head over to System Preferences and check `Multiliner`.")
                             .font(.title3)
-
-                        Image("UsageCode")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
+                    
+                        Screenshot(image: "Preferences")
+                    
+                        Text("System Preferences → Extensions → Xcode Source Editor")
+                            .font(.caption)
+                            .textCase(.uppercase)
                     }
-                    
-                    Spacer().frame(height: 25)
-                    
-                    Group {
-                        Text("2. Then, go to Editor → Multiliner → Format Selected Code. Your code will get reformatted!")
-                            .font(.title3)
-
-                        HStack {
-                            Image("UsageMenu")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-
-                            Image("UsageResult")
+                
+                    VStack(alignment: .leading, spacing: 20) {
+                        VStack(alignment: .leading) {
+                            Text("Usage")
+                                .font(.title2)
+                                .bold()
+                        
+                            Text("1. Select some code in Xcode. Make sure to include the opening and closing braces.")
+                                .font(.title3)
+                        
+                            Image("UsageCode")
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
                         }
-                    }
                     
-                    Spacer().frame(height: 25)
+                        VStack(alignment: .leading) {
+                            Text("2. Then, go to Editor → Multiliner → Format Selected Code. Your code will get reformatted!")
+                                .font(.title3)
+                        
+                            HStack(alignment: .top) {
+                                Image("UsageMenu")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .cornerRadius(8)
+                                    .shadow(
+                                        color: .black.opacity(0.25),
+                                        radius: 16,
+                                        x: 0,
+                                        y: 2
+                                    )
+                                    
+                                Image("UsageResult")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .cornerRadius(8)
+                                    .shadow(
+                                        color: .black.opacity(0.25),
+                                        radius: 16,
+                                        x: 0,
+                                        y: 2
+                                    )
+                            }
+                        }
                     
-                    Group {
-                        Text("3. To make this extension easier to reach, consider adding a shortcut in Xcode's Key Bindings.")
-                            .font(.title3)
-
-                        Image("UsageShortcut")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
+                        VStack(alignment: .leading) {
+                            Text("3. To make this extension easier to reach, consider adding a shortcut in Xcode's Key Bindings.")
+                                .font(.title3)
+                        
+                            Screenshot(image: "UsageShortcut")
+                        
+                            Text("Xcode → Preferences → Key Bindings")
+                                .font(.caption)
+                                .textCase(.uppercase)
+                        }
                     }
                 }
+                
+                Divider()
+                
+                Text("That's it! If you have any questions or suggestions, feel free to ping me on Twitter ([@aheze0](https://twitter.com/aheze0)), join the [Discord](https://discord.gg/Pmq8fYcus2), or [open an issue](https://github.com/aheze/Multiliner/issues).")
             }
+            .frame(maxWidth: 800)
             .padding(.horizontal, 64)
             .padding(.vertical, 32)
+            .frame(maxWidth: .infinity)
         }
         .frame(
             minWidth: 400,
@@ -115,6 +139,19 @@ struct ContentView: View {
                 }
             }
         }
+    }
+}
+
+struct Screenshot: View {
+    var image: String
+    var body: some View {
+        Image(image)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(maxWidth: 600)
+            .padding(.horizontal, -42)
+            .padding(.top, -20)
+            .padding(.bottom, -40)
     }
 }
 
